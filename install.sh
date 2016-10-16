a0dc0d15dfe447c5da30ccfad735556f40303378
Albert Serrallé Ríos <aserrallerios@gmail.com>
2016-10-16 12:44:15 +0200

    Cleaning stuff

--- install.sh
+++ install.sh
@@ -23,14 +23,6 @@ ln -s $DOTFILES_FOLDER/vim $HOME/.vim
 ln -s $DOTFILES_FOLDER/gitconfig $HOME/.gitconfig
 ln -s $DOTFILES_FOLDER/gitignore $HOME/.gitignore
 
-wget https://storage.googleapis.com/kubernetes-release/release/v1.3.0/bin/linux/amd64/kubectl -O $HOME/bin
-
-mv $HOME/.config/terminator $HOME/.config/terminator_old
-ln -s $DOTFILES_FOLDER/config/terminator $HOME/.config/
-
-mv $HOME/.zsh_history $HOME/.zsh_history_old
-ln -sf $DOTFILES_FOLDER/zsh_history $HOME/.zsh_history
-
 ln -s $DOTFILES_FOLDER/oh-my-zsh $HOME/.oh-my-zsh
 mkdir $HOME/.oh-my-zsh/custom/themes 2> /dev/null
 ln -s $DOTFILES_FOLDER/custom.zsh-theme $HOME/.oh-my-zsh/custom/themes/custom.zsh-theme
@@ -39,5 +31,4 @@ ln -s $DOTFILES_FOLDER/rbenv $HOME/.rbenv
 mkdir $HOME/.rbenv/plugins
 ln -s $DOTFILES_FOLDER/ruby-build $HOME/.rbenv/plugins/ruby-build
 
-source $HOME/.zshrc
 chsh -s `which zsh`
